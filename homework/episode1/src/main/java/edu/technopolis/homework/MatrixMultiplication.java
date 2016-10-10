26e823818a9649516239d5e7257223b75e9e17d9
TimurTechnopolis <timur.technopolis@mail.ru>
2016-10-10 10:48:43 +0300

    Introduction of episode 1 home task.

---
+++ homework/episode1/src/main/java/edu/technopolis/homework/MatrixMultiplication.java
@@ -0,0 +1,12 @@
+package edu.technopolis.homework;
+
+/**
+ * Matrix multiplication home task.
+ * <br/>
+ * Matrix dimension and elements are passed as CLI arguments.
+ */
+public class MatrixMultiplication {
+    public static void main(String... args) {
+
+    }
+}
