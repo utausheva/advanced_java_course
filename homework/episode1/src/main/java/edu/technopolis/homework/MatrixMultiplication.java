b48c01a1a6033d71bc23fe0ac1fce4cad75ec113
utausheva <ulyashk@list.ru>
2017-02-01 11:59:20 +0300

    Update MatrixMultiplication.java

--- homework/episode1/src/main/java/edu/technopolis/homework/MatrixMultiplication.java
+++ homework/episode1/src/main/java/edu/technopolis/homework/MatrixMultiplication.java
@@ -10,23 +10,17 @@ import java.util.Scanner;
  * <br/>
  * Matrix dimension and elements are passed as CLI arguments.
  */
-public class MatrixMultiplication {
-     static void Multi(int[][] a, int[][] b) {
+public class MultiMatrix {
+    public static void main(String[] args) throws Throwable {
 
-        int[][] res = new int[a.length][b[0].length];
-
-        for (int i = 0; i < a.length; i++) {
-            for (int j = 0; j < b[0].length; j++) {
-                for (int k = 0; k < a[0].length; k++) {
-                    res[i][j] += a[i][k] * b[k][j];
-                }
-                System.out.print(res[i][j] + " ");
-            }
-            System.out.println();
-        }
-    }
-    public static void main(String... args) throws Throwable {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Программа считает произведение матриц.");
+        System.out.println("Введите размерности матриц и матрицы в формате:");
+        System.out.println("N M X Y A_1_1 ... A_N_M B_1_1 ... B_X_Y");
+        System.out.println("где N и M - размерность первой матрицы A (количество строк и столбцов),");
+        System.out.println("A_1_1 ... A_N_M - элементы матрицы A, ");
+        System.out.println(" X и Y - размерность второй матрицы B,");
+        System.out.println("B_1_1 ... B_X_Y - элементы матрицы B.");
 
         //Считывание размерностей матриц
         try {
@@ -35,17 +29,32 @@ public class MatrixMultiplication {
             int x = sc.nextInt();
             int y = sc.nextInt();
 
-            if (n <= 0 || m <= 0 || x <= 0 || y <= 0)
-                throw new IOException("Неправильно введены размерности массивов!");
-            if (m != x)
-                throw new SingularMatrixException("Не перемножить матрицы, поскольку размерности не соотносятся");
-
-
+            boolean flag = false;
+            while (!flag) {
+                if (n <= 0 || m <= 0 || x <= 0 || y <= 0) {
+                    // throw new IOException("Неправильно введены размерности массивов!");
+                    System.out.println("Размерности - положительные целые числа!");
+                    n = sc.nextInt();
+                    m = sc.nextInt();
+                    x = sc.nextInt();
+                    y = sc.nextInt();
+                } else flag = true;
+                if (m != x) {
+                    //throw new SingularMatrixException("Не перемножить матрицы, поскольку размерности не соотносятся");
+                    System.out.println("Не перемножить матрицы, поскольку размерности не соотносятся (введите m==x)");
+                    System.out.println("Заново повторите ввод размерностей:");
+                    n = sc.nextInt();
+                    m = sc.nextInt();
+                    x = sc.nextInt();
+                    y = sc.nextInt();
+                } else flag = true;
+            }
             //Инициализация матриц
             int[][] a = new int[n][m];
             int[][] b = new int[x][y];
 
             //Считывание первой матрицы
+            System.out.println("Введите " + n*m + " чисел первой матрицы");
             for (int i = 0; i < n; i++) {
                 for (int j = 0; j < m; j++) {
                     a[i][j] = sc.nextInt();
@@ -53,16 +62,27 @@ public class MatrixMultiplication {
             }
 
             //Считывание второй матрицы
+            System.out.println("Введите " + x*y + " чисел второй матрицы");
             for (int i = 0; i < x; i++) {
                 for (int j = 0; j < y; j++) {
                     b[i][j] = sc.nextInt();
                 }
             }
 
-            Multi(a, b);
+            int[][]res= new int[n][y];
+
+            for (int i = 0; i < a.length; i++) {
+                for (int j = 0; j < b[0].length; j++) {
+                    for (int k = 0; k < a[0].length; k++) {
+                        res[i][j] += a[i][k] * b[k][j];
+                    }
+                    System.out.print(res[i][j] + " ");
+                }
+                System.out.println();
+            }
 
         } catch (InputMismatchException e) {
-            System.out.println("Размерность матрицы - не число!");
+            System.out.println("Размерность матрицы - не число, либо дробное число!");
         }
     }
 }
